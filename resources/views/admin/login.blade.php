a276023b0932d83966195603394c0e35ed7349e5
Jsvrns <72838950+JesaViernes033@users.noreply.github.com>
2021-12-03 09:03:45 +0800

    login page update

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css')}}" >
+    <link rel="stylesheet" href="/css/app.css" >
   
   
   </head>
@@ -18,8 +18,9 @@
   >
     
 
-      <div id="login-card" class="card">
-          <div class="card-body">
+        <div id="login-card" class="card">
+          
+            <div class="card-body">
                 <div class="logo">
                     <img class="text-center" src="https://scontent.fmnl9-3.fna.fbcdn.net/v/t1.15752-9/262305527_426710942357529_7743025437953405122_n.png?_nc_cat=106&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeEgv9Cm0JpyYTnYeLC-R2zHDIETr2mh02QMgROvaaHTZFI_uaU6uazdUddTvkmhKGk&_nc_ohc=QkeCIfvR54AAX_NyBQL&_nc_ht=scontent.fmnl9-3.fna&oh=7fd81b0deea6d100b1b480514230c18e&oe=61CDCBD3" alt="My test image">
                 </div>
