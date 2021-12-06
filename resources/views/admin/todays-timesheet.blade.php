f9531fd763242b21f46e389ab20760f4d2f5d935
Jsvrns <72838950+JesaViernes033@users.noreply.github.com>
2021-12-06 22:30:09 +0800

    CSS

--- resources/views/admin/todays-timesheet.blade.php
+++ resources/views/admin/todays-timesheet.blade.php
@@ -15,7 +15,7 @@
 		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">		
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/timesheet.css')}}" >
+    <link rel="stylesheet" href="{{url('css/timesheets.css')}}">
 	</head>
 
   <body>
