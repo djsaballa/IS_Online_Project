8f5dba7c7d8eb6e65b359ee1e03837c7f917ca9e
Jsvrns <72838950+JesaViernes033@users.noreply.github.com>
2021-12-14 12:30:58 +0800

    title

--- resources/views/admin/view-employees.blade.php
+++ resources/views/admin/view-employees.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Hima Corps Inc. | Attendance Monitoring Systemt</title>
+		<title>Hima Corps Inc. | Attendance Management System</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
 			integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
@@ -28,7 +28,7 @@
           <div class="logo">
             <img class="text-center" src="https://scontent.fmnl9-3.fna.fbcdn.net/v/t1.15752-9/262305527_426710942357529_7743025437953405122_n.png?_nc_cat=106&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeEgv9Cm0JpyYTnYeLC-R2zHDIETr2mh02QMgROvaaHTZFI_uaU6uazdUddTvkmhKGk&_nc_ohc=QkeCIfvR54AAX_NyBQL&_nc_ht=scontent.fmnl9-3.fna&oh=7fd81b0deea6d100b1b480514230c18e&oe=61CDCBD3" alt="My test image">
           </div>
-          <h5 style="text-align: center;">Admin Attendance Management System</h5>
+          <h5 style="text-align: center;">Attendance Management System</h5>
         </div>
 
         <ul class="lisst-unstyled components">
