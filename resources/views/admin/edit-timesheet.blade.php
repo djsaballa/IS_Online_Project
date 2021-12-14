a8f7e0c2a6e69b42750316de1574ed2a758c9782
Jsvrns <72838950+JesaViernes033@users.noreply.github.com>
2021-12-15 01:59:15 +0800

    edit-timesheets

--- resources/views/admin/edit-timesheet.blade.php
+++ resources/views/admin/edit-timesheet.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Hima Corps Inc. | Attendance Management System</title>
+		<title>Hima Corps Inc. | Attendance Monitoring Systemt</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
 			integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
@@ -10,64 +10,116 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
 			crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-			crossorigin="anonymous"></script>
-			<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
-		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">		
+			crossorigin="anonymous"></script>		
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Styles -->
-    <link rel="stylesheet" href="{{url('css/timesheets.css')}}">
+    <link rel="stylesheet" href="{{url('css/edit-timesheet.css')}}">
 	</head>
 
   <body>
 
     <div class="wrapper">
 
-      <nav id="sidebar">
+        <nav id="sidebar">
 
-        <div class="sidebar-header">
-          <div class="logo">
-            <img class="text-center" src="https://scontent.fmnl9-3.fna.fbcdn.net/v/t1.15752-9/262305527_426710942357529_7743025437953405122_n.png?_nc_cat=106&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeEgv9Cm0JpyYTnYeLC-R2zHDIETr2mh02QMgROvaaHTZFI_uaU6uazdUddTvkmhKGk&_nc_ohc=QkeCIfvR54AAX_NyBQL&_nc_ht=scontent.fmnl9-3.fna&oh=7fd81b0deea6d100b1b480514230c18e&oe=61CDCBD3" alt="My test image">
+          <div class="sidebar-header">
+            <div class="logo">
+              <img class="text-center" src="https://scontent.fmnl9-3.fna.fbcdn.net/v/t1.15752-9/262305527_426710942357529_7743025437953405122_n.png?_nc_cat=106&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeEgv9Cm0JpyYTnYeLC-R2zHDIETr2mh02QMgROvaaHTZFI_uaU6uazdUddTvkmhKGk&_nc_ohc=QkeCIfvR54AAX_NyBQL&_nc_ht=scontent.fmnl9-3.fna&oh=7fd81b0deea6d100b1b480514230c18e&oe=61CDCBD3" alt="My test image">
+            </div>
+            <h5 style="text-align: center;">Attendance Management System</h5>
           </div>
-          <h5 style="text-align: center;"> Attendance Management System</h5>
-        </div>
-        
-        <ul class="lisst-unstyled components">
-          <li>
-            <a href="{{ route('employee_take_attendance', $employee_info->id) }}" >
-            <span><i class="fas fa-user-clock" style="margin-left:30px;"></i></span> Take Attendance</a>
-          </li>
-          <li>
-            <a href="{{ route('employee_view_timesheets', $employee_info->id) }}">
-            <span><i class="fas fa-calendar-week" style="margin-left:30px; width: 10px; margin-right:10px;"></i></span> View Timesheets</a>
-          </li>
-        </ul>
-        <div class="sidebar-bottom">	
-          <div class="logout">
-            <a href="/">
-            <i class="fas fa-sign-out-alt" style="margin-left:30px; "></i> Logout</a>
+          
+          <ul class="lisst-unstyled components">
+            <li>
+              <a href="{{ route('admin_view_employees') }}" >
+              <span><i class="fas fa-user-clock" style="margin-left:30px;"></i></span> Take Attendance</a>
+            </li>
+            <li>
+              <a href="{{ route('admin_todays_timesheet') }}">
+              <span><i class="fas fa-calendar-week" style="margin-left:30px; width: 10px; margin-right:10px;"></i></span> View Timesheets</a>
+            </li>
+          </ul>
+          <div class="sidebar-bottom">	
+            <div class="logout">
+              <a href="/admin/login">
+              <i class="fas fa-sign-out-alt" style="margin-left:30px; "></i> Logout</a>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
 
       <div id="content">
 
         <nav class="navbar bg-light">
-          <div class="container-fluid">
-            <button type="button" id="sidebarCollapse" class="btn btn-outline-info">
-              <i class="fa fa-align-left"></i>
-            </button>
+            <div class="container-fluid">
+              <button type="button" id="sidebarCollapse" class="btn btn-outline-info btn-up">
+                <i class="fa fa-align-left"></i>
+              </button>
             <div id="up">
-              <span style="margin-left:-450px;"><i class="far fa-calendar-alt" style="margin-right:5px; font-size:19px"></i></span>View Timesheets</a>
+              <span style="margin-left:-450px;"><i class="fas fa-edit" style="margin-right:5px; font-size:19px"></i></span>Edit Timesheet</a>
             </div>	
             <div id="admin">
-              <span> {{ $employee_info->getFirstLast() }} </span> <img style="width:50px; height:30px; " src="https://image.pngaaa.com/583/3999583-middle.png" 
+              <span> Administrator </span> <img style="width:25px; height:25px; " src="https://png.pngitem.com/pimgs/s/4-40070_user-staff-man-profile-user-account-icon-jpg.png" 
                 alt="Avatar">
             </div>
           </div>
         </nav>
 
-        
-       
+        <br><br>
+
+         
+        <div class="wrap">
+          <h1>Edit Timesheet</h1> 
+          <div class="form-center">
+              <form style="margin-left: 250px;"> 
+                  <div class="form-group row" >
+                    <label for="staticEmail" class="col-sm-2 col-form-label" style="font-weight: bold;">Email:</label>
+                    <div class="col-sm-10">
+                      <input type="text" readonly class="form-control-plaintext" id="email" >
+                    </div>
+                  </div>
+
+                  <div class="form-group row">
+                    <label  class="col-sm-2 col-form-label" style="font-weight: bold;">Date</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="date" style="width:250px;"> 
+                    </div>
+                  </div>
+
+                  <div class="form-group row">
+                    <label  class="col-sm-2 col-form-label" style="font-weight: bold; ;">Time in</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="TimeIn"  style="width:250px; ">
+                    </div>
+                  </div>
+
+                  <div class="form-group row">
+                    <label  class="col-sm-2 col-form-label" style="font-weight: bold;">Lunch Start</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="lunchStart" style="width:250px; ">
+                    </div>
+                  </div>
+
+                  <div class="form-group row">
+                    <label  class="col-sm-2 col-form-label" style="font-weight: bold;">Lunch End</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="lunchEnd" style="width:250px;">
+                    </div>
+                  </div>
+
+                  <div class="form-group row ">
+                    <label  class="col-sm-2 col-form-label" style="font-weight: bold;">Time out</label>
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="TimeOut" style="width:250px;" >
+                    </div>
+                  </div>
+                  <button type="button"  class="btn btn-Secondary button1 ">Cancel</button>
+                  <button type="button"  class="btn btn-primary button  ">Save</button>
+                  
+              </form>
+            </div>
+        </div>
+      </div>
+    </div>
 
       <script>
 
@@ -76,11 +128,7 @@
             $('#sidebar').toggleClass('active');
           });
         });
-
-        $(document).ready( function () {
-            $('#table_id').DataTable();
-        } );
-
+        
       </script>
 
   </body>
