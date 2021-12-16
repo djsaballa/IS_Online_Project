45baf57335a8f43e876199a461e92022c894d20f
Jsvrns <72838950+JesaViernes033@users.noreply.github.com>
2021-12-16 21:14:29 +0800

    logo

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -13,8 +13,8 @@
     <div id="login-card" class="card">
       <div class="card-body">
         <div class="logo">
-            <img class="text-center" src="https://scontent.fmnl9-3.fna.fbcdn.net/v/t1.15752-9/262305527_426710942357529_7743025437953405122_n.png?_nc_cat=106&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeEgv9Cm0JpyYTnYeLC-R2zHDIETr2mh02QMgROvaaHTZFI_uaU6uazdUddTvkmhKGk&_nc_ohc=QkeCIfvR54AAX_NyBQL&_nc_ht=scontent.fmnl9-3.fna&oh=7fd81b0deea6d100b1b480514230c18e&oe=61CDCBD3" alt="My test image">
-        </div>
+            <img src="{{url('/images/logo.png')}}" alt="Image"/>
+          </div>
         <div class="title"> Welcome, Admin! </div>
         <div class="sub-title"> Please enter your username and password to continue. </div>
 
